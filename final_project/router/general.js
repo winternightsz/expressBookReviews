d3dc7930da6ecfb4e9834240be6b46919daf5597
winternightsz <izabelaandreazza@gmail.com>
2025-10-31 22:54:45 -0400

    Third Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,37 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// Register a new user
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -63,19 +90,12 @@ public_users.get('/title/:title',function (req, res) {
     res.send(JSON.stringify(titledBooks,null,4));
 });
 
-//  Get book review
+//  Get book review. Hint: Get the book reviews based on ISBN provided in the request parameters.
 public_users.get('/review/:isbn',function (req, res) {
     const ISBN = req.params.isbn;
-    Object.keys(books).forEach(outerKey => {
-        const innerObject = books[outerKey];
-    
-        // Verifies if the current books equals the requested book
-        if (innerObject.isbn === ISBN) {
-          const review = innerObject.review;
-          res.send(JSON.stringify(review,null,4));
-          return;
-        }
-      });
+    const book = books[ISBN];
+    const review = book.reviews;
+    res.send(JSON.stringify(review,null,4));
 });
 
 module.exports.general = public_users;

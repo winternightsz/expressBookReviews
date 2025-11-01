d9729de45ac9cfa3b81de932563413e9b867f542
winternightsz <izabelaandreazza@gmail.com>
2025-10-31 22:39:22 -0400

    Second Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,70 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+        // Retrieve the email parameter from the request URL and send the corresponding friend's details
+        const ISBN = req.params.isbn;
+        res.send(books[ISBN]);  
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    let authorBooks = {};
+    const reqAuthor = req.params.author;
+  
+    // Goes through all object keys
+    Object.keys(books).forEach(outerKey => {
+      const innerObject = books[outerKey];
+  
+      // Verifies if the current books equals the requested book
+      if (innerObject.author.toLowerCase() === reqAuthor.toLowerCase()) {
+        // Adds the book to the response
+        authorBooks[outerKey] = innerObject;
+      }
+    });
+  
+    // Returns response
+    res.send(JSON.stringify(authorBooks,null,4));
+  });
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let titledBooks = {};
+    const reqTitle = req.params.title;
+  
+    // Goes through all object keys
+    Object.keys(books).forEach(outerKey => {
+      const innerObject = books[outerKey];
+  
+      // Verifies if the current books equals the requested book
+      if (innerObject.title.toLowerCase() === reqTitle.toLowerCase()) {
+        // Adds the book to the response
+        titledBooks[outerKey] = innerObject;
+      }
+    });
+  
+    // Returns response
+    res.send(JSON.stringify(titledBooks,null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const ISBN = req.params.isbn;
+    Object.keys(books).forEach(outerKey => {
+        const innerObject = books[outerKey];
+    
+        // Verifies if the current books equals the requested book
+        if (innerObject.isbn === ISBN) {
+          const review = innerObject.review;
+          res.send(JSON.stringify(review,null,4));
+          return;
+        }
+      });
 });
 
 module.exports.general = public_users;
